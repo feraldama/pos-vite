b89163f0f84562c02a82d37a3c36f3653f9bdfaf
fernando-aldama-itti <fernando.aldama@itti.digital>
2025-06-12 13:04:12 -0300

    mejora en la gestión de entradas de texto en los componentes AlmacenesList, CajasList, LocalesList y UsersList: se actualiza la lógica de onChange para convertir el texto a mayúsculas antes de manejar el cambio.

--- client/src/components/cajas/CajasList.tsx
+++ client/src/components/cajas/CajasList.tsx
@@ -195,7 +195,15 @@ export default function CajasList({
                       name="CajaDescripcion"
                       id="CajaDescripcion"
                       value={formData.CajaDescripcion}
-                      onChange={handleInputChange}
+                      onChange={(e) => {
+                        const value = e.target.value.toUpperCase();
+                        handleInputChange({
+                          target: {
+                            name: "CajaDescripcion",
+                            value: value,
+                          },
+                        } as React.ChangeEvent<HTMLInputElement>);
+                      }}
                       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                       required
                     />
